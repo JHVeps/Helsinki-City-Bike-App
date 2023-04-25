b8345591e01a2aa48ac9432035a4eced50dcfb7f
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-04-25 16:12:41 +0300

    logic for importing journey in routes/journey.import.ts

--- import_api/src/routes/journey.import.ts
+++ import_api/src/routes/journey.import.ts
@@ -1,5 +1,66 @@
-import express from "express";
+import multer from "multer";
+import express, { Request, Response } from "express";
+import JourneyModel from "../models/journey.model";
+import csv from "csvtojson";
+import type { Journey } from "../types";
+
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
+
+router.get("/", (_req, res) => {
+  JourneyModel.find({}, (error: Error, items: any) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json({ items: items });
+    }
+  });
+});
+
+router.post("/", upload.single("file"), (req: Request, res: Response) => {
+  if (req.file) {
+    csv()
+      .fromFile(req.file.path)
+      .then((jsonObj: any[]) => {
+        const journeys = [];
+
+        for (let i = 0; i < jsonObj.length; i++) {
+          const journeyObj = {} as Journey;
+
+          const durationToParse = (journeyObj.Duration =
+            jsonObj[i]["Duration (sec"]);
+
+          const extractedDuration = durationToParse[")"];
+
+          journeyObj.Departure = jsonObj[i]["Departure"];
+          journeyObj.Return = jsonObj[i]["Return"];
+          journeyObj.DepartureStationId = jsonObj[i]["Departure station id"];
+          journeyObj.DepartureStationName =
+            jsonObj[i]["Departure station name"];
+          journeyObj.ReturnStationId = jsonObj[i]["Return station id"];
+          journeyObj.ReturnStationName = jsonObj[i]["Return station name"];
+          journeyObj.CoveredDistance = jsonObj[i]["Covered distance (m)"];
+          journeyObj.Duration = extractedDuration;
+
+          if (journeyObj.CoveredDistance > 10 && journeyObj.Duration > 10) {
+            journeys.push(journeyObj);
+          }
+        }
+        JourneyModel.insertMany(journeys)
+          .then(function () {
+            res.status(200).send({
+              message: "Successfully Uploaded!",
+            });
+          })
+          .catch((error: Error) => {
+            res.status(500).send({
+              message: "failure",
+              error,
+            });
+          });
+      });
+  }
+});
 
 export { router };
