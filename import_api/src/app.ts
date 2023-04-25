4dd3942153c73eb68bac54d440d8001d3c7ef409
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-04-25 15:18:35 +0300

    added initial route for importing journey csv file

--- import_api/src/app.ts
+++ import_api/src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { router as importJourney } from "./routes/journey.import";
 
 dotenv.config();
 
@@ -13,5 +14,7 @@ app.use(
     origin: "*",
   })
 );
+
+app.use("/api/v1/import/journeys", importJourney);
 
 export default app;
