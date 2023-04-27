e1e65df4656956d4bd80f68c72f8a5e00b815f6c
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-04-27 12:22:10 +0300

    App -> routing for Home/Station views

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,12 +1,19 @@
-import Home from "../pages/home/Home";
+import Stations from "components/pages/stations/Stations";
+import Home from "components/pages/home/Home";
 
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <h1>HELSINKI CITY BIKE APP</h1>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stations" element={<Stations />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
