3aea15c204f04addb00433f98238e2dd5b14d777
JHVeps <90003389+JHVeps@users.noreply.github.com>
2023-11-10 17:06:18 +0200

    applied middlewares to routes

--- api/src/routers/user.router.ts
+++ api/src/routers/user.router.ts
@@ -12,6 +12,7 @@ const router = express.Router();
 router.get("/all", requireSignin, findAllUsers);
 router.get("/user/:id", requireSignin, findById);
 router.get("/user/email/:email", requireSignin, findByEmail);
-router.put("/user/update", requireSignin, adminMiddleware, updateUser);
+router.put("/user/update", requireSignin, updateUser);
+router.put("/admin/update", requireSignin, adminMiddleware, updateUser);
 
 export default router;
